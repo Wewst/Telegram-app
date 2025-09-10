61ad338cad88bfae558fc603a02755da99ba098b
Wewst <ggbdhdj663@gmail.com>
2025-09-10 07:56:53 +0500

    Update index.js

--- index.js
+++ index.js
@@ -43,6 +43,14 @@ loadDB();
 // Автосохранение каждые 30 секунд
 setInterval(saveDB, 30000);
 
+// Разрешаем все CORS запросы
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 // --- CORS Middleware ---
 app.use(cors({
   origin: "*",
